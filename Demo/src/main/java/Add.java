0493a6bcb75f0fee7488378a86d0252e59640a6c
JohnPole <johnpole555@gmail.com>
2021-06-16 01:55:52 -0400

    First commit uploading all files

---
+++ Demo/src/main/java/Add.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author johnv
+ *
+ */
+public class Add {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi");
+
+	}
+
+}
